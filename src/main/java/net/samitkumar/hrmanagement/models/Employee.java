8f8b81d6e3fbb28c2c81011c9c32132b7c240c3c
samitkumarpatel <samitkumarpatel@gmail.com>
2024-08-24 23:10:37 +0200

    aggrigate root added for managerId

--- src/main/java/net/samitkumar/hrmanagement/models/Employee.java
+++ src/main/java/net/samitkumar/hrmanagement/models/Employee.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -21,13 +22,13 @@ public record Employee(
         @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate hireDate,
         Integer jobId,
         Double salary,
-        Integer managerId,
+        AggregateReference<Employee, Integer> managerId,
         Integer departmentId,
         @MappedCollection(idColumn = "employee_id") EmployeeHistory employeeHistory,
         @MappedCollection(idColumn = "employee_id") Set<EmployeeDocument> employeeDocuments,
         @JsonIgnore @Transient Object extraProperty) {
     @PersistenceCreator
-    public Employee(Integer employeeId, String firstName, String lastName, String email, String phoneNumber, LocalDate hireDate, Integer jobId, Double salary, Integer managerId, Integer departmentId, EmployeeHistory employeeHistory, Set<EmployeeDocument> employeeDocuments) {
+    public Employee(Integer employeeId, String firstName, String lastName, String email, String phoneNumber, LocalDate hireDate, Integer jobId, Double salary, AggregateReference<Employee, Integer> managerId, Integer departmentId, EmployeeHistory employeeHistory, Set<EmployeeDocument> employeeDocuments) {
         this(employeeId, firstName, lastName, email, phoneNumber, hireDate, jobId, salary, managerId, departmentId, employeeHistory, employeeDocuments, null);
     }
 }
