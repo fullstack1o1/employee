d02243dacaffa8e5c608de3d6701f09a510f75c4
samitkumarpatel <samitkumarpatel@gmail.com>
2025-05-25 22:08:45 +0200

    bug fixses

--- src/test/java/net/samitkumar/employee/EmployeeTestApplication.java
+++ src/test/java/net/samitkumar/employee/EmployeeTestApplication.java
@@ -2,15 +2,14 @@ package net.samitkumar.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Import;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class EmployeeManagementApplication {
+public class EmployeeTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.from(EmployeeApplication::main)
 				.with(TestcontainersConfiguration.class)
-				.with(EmployeeManagementApplication.class)
+				.with(EmployeeTestApplication.class)
 				.run(args);
 	}
 
